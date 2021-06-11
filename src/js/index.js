2d6d5a422ba33d004d3839668b805ac45f372597
Igor3007 <50610898+Igor3007@users.noreply.github.com>
2021-06-11 16:36:43 +0300

    init

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+import "./import/modules";
+import "./import/components";
+import svgPolyfill from "../../node_modules/svg4everybody/dist/svg4everybody.js";
+import Swiper, {
+  Pagination,
+  Navigation,
+  Thumbs,
+  Autoplay,
+} from 'swiper';
+import 'jquery.inputmask/dist/jquery.inputmask.bundle';
+
+import $ from 'jquery';
+import './import/jquery.fancybox.min';
+
+svgPolyfill();
+
+$(document).ready(function () {
+  
+
+});
